2bb84a87c0a8b0c4fceff1360ebdb89e78776b17
springo21 <springodigbo@gmail.com>
2024-03-01 10:56:35 +0100

    Initial commit

---
+++ midterm.py
@@ -0,0 +1,66 @@
+# a = 10
+# a = a + 2
+# print(a*2)
+# a = 19
+# print(a+3)
+# a = a // 2
+# print(a)
+from string import punctuation
+
+# print((2+3)*6/2 and 18.0)
+
+# import datetime
+
+# a = 7
+# b = 2
+# today = datetime.datetime.today()
+# day_of_week = today.weekday()
+# month_of_year = today.month
+# a = a + day_of_week
+# b += month_of_year
+#
+# print(a)
+# print(b)
+# c = a + b
+# print(c)
+# d = "xyz" * (c // 3)
+# print(d)
+
+# def palindrome(word):
+#    if word == word[::-1]:
+#        print("That is a palindrome")
+#        return True
+#    else:
+#        print("That is not a palindrome")
+#        return False
+
+
+# palindrome("7798338247658278460338648728567428338977")
+
+# file_names = "midterm_exam.txt"
+# fd = open(file_names)
+
+
+# def words_C_jeb(filename):
+#    with open(file_names) as fd:
+#        print("here are the words that start with C and end with -jeb! ")
+#       for line in fd:
+#            for p in punctuation:
+#               line = line.replace(p, "")
+#            words = line.split()
+#            for word in words:
+#                if word.startswith("C") and word.endswith('jeb'):
+#                    print(word)
+
+
+# fd.close()
+
+# words_C_jeb("midterm_exam.txt")
+
+# dogs = ['yorkie', 'daschund', 'schnauzer', 'golden retriever']
+# dogs.append("pomeranian")
+# print(dogs)
+
+word = "happi"
+word.append('ness')
+
